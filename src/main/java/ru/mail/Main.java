a22952d5517a5c37dbcc6481ba8fd5deb19c6452
m.mezentseva <mezya@mail.ru>
2023-01-07 18:57:42 +0700

    Материалы занятия

--- src/main/java/ru/mail/Main.java
+++ src/main/java/ru/mail/Main.java
@@ -1,5 +1,55 @@
-package ru.mail;public class Main {
+package ru.mail;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //целые числа
+        byte varByte = 1; // -128 ... 127
+        short varShort = 1; //-32768 ... 32767
+        //must have
+        int varInt = 1;
+        long varLong = 1L;
+
+        //числа с плавающей точкой
+        float varFloat = 0.0f;
+        //must have
+        double varDouble = 0.0d;
+
+        //логический
+        boolean varBool = true;
+
+        //символьный
+        char varChar = 'с';
+
+        //ссылочные (объектные) типы данных - классы
+        String varString = "Hello, qa.guru";
+
+        //ОПЕРАТОРЫ
+        //1. Присвоения
+        int varInt0 = 0;
+        int varInt0 += 10; //используется в краткой форме редко
+
+        //2. Математические (+, -, *, /, %, ++, --)
+        varInt0 = 7 / 4; //в java деление - целочисленное, то есть остаток будет выброшен
+        varInt0 = 7 % 4; //остаток от деления, тут 3
+        varInt0 = ++varInt; //префиксная запись инкремента
+        varInt0 = varInt--; //постфиксная запись декремента
+
+        //3. Сравнения (>, <, >=, <=, !=, ==)
+        if (1 == 1) // не используется для ссылочных типов
+
+        //4. Логические операторы (И &&, ИЛИ ||, не !)
+        if ((age >= 30) && (sex.equals("female")))
+        if (!((age >= 30) && (sex.equals("female")))) //изменяет true на false
+
+        if (varString != null && varString.length() == 10) {
+            System.out.println(varString);
+        } else {
+            System.out.println("Извините, строка не из 10 символов");
+        }
+
+
+
+        System.out.println("Hello, qa.guru");
+
     }
 }
